9f2b10c0e4b1eed0e4c5b7d749dc16dd26178178
NightmareNyx <nightmare7777777@gmail.com>
2016-01-01 11:45:06 +0200

    renaming

--- logic.py
+++ logic.py
@@ -18,6 +18,8 @@ class Term(object):
 
 
 class Variable(Term):
+    new_num = 0  # "static" member to be used in produce_new_name function
+
     def __init__(self, name):
         super(Variable, self).__init__(name)
 
@@ -38,17 +40,20 @@ class Variable(Term):
     def get_bindings(self, bind_dict):
         # vars_dict a dictionary -> variable:binding_values
         binding = bind_dict.get(self)
-
         closed_set = [self, binding]
         while isinstance(binding, Variable) and binding in bind_dict and bind_dict[binding] not in closed_set:
             binding = bind_dict.get(binding)
             closed_set.append(binding)
-
         # expand the bound relation
         if isinstance(binding, Relation):
             return binding.make_bindings(bind_dict)
-
         return binding
+
+    @staticmethod
+    def produce_new_name(self):
+        # produce a new temporary name to avoid confusion between variable names
+        Variable.new_num += 1
+        return Variable('%s%d' % (self.name, Variable.new_num))
 
 
 class Relation(Term):
@@ -71,8 +76,16 @@ class Relation(Term):
                 bound.append(arg.get_bindings(bind_dict))
             else:
                 bound.append(arg)
-
         return Relation(self.name, bound)
+
+    def produce_new_names4vars(self):
+        new_names = []
+        for arg in self.args:
+            new_names.append(Variable.produce_new_name())
+        return new_names
+
+    def rename_vars(self):
+        return Relation(self.name, Relation.produce_new_names4vars())
 
 
 class Clause(Term):
@@ -96,8 +109,19 @@ class Clause(Term):
         body = []
         for rel in self.body:
             body.append(rel.make_bindings(bind_dict))
-
         return Clause(head, body)
+
+    def produce_new_names4vars(self):
+        renamed_body = []
+        for part in self.body:
+            renamed_body.append(part.rename_vars())
+        return renamed_body
+
+    def rename_vars(self):
+        return Clause(self.head.rename_vars(), self.produce_new_names4vars())
+
+
+# renaming to be used in proving goals
 
 
 class List(Term):
@@ -213,8 +237,9 @@ def fol_bc_ask(KB, goals, unifier):
 
     for t in KB:
         t = t.new_vars()  # TODO
-        new_unif = unify( t.head , b)
-        if not new_unif : continue
+        new_unif = unify(t.head, b)
+        if not new_unif:
+            continue
 
         goals.extend(t.body)  # to extend einai gia na pros8eseis ta stoixeia
         # mias listas se iparxousa lista
